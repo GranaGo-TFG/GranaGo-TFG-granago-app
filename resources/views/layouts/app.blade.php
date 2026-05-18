c24597cd0142693d0878587dad7c312bc701ad4a
Pedro <pedrotorres001.alu@stemgranada.com>
2026-05-18 22:33:22 +0200

    Modifico el blade de app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @stack('styles')
 </head>
 <body>
     <div id="app">
@@ -110,5 +111,7 @@
             @yield('content')
         </main>
     </div>
+
+    @stack('scripts')
 </body>
 </html>
