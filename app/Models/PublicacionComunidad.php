481e338cde09790b4567ac61636e7b112b086694
Pedro <pedrotorres001.alu@stemgranada.com>
2026-05-25 17:35:20 +0200

    Modifico modelo

--- app/Models/PublicacionComunidad.php
+++ app/Models/PublicacionComunidad.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PublicacionComunidad extends Model
@@ -37,5 +38,11 @@ class PublicacionComunidad extends Model
     public function comentarios(): HasMany
     {
         return $this->hasMany(ComentarioComunidad::class, 'publicacion_id');
+    }
+
+    public function meGustaUsuarios(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'publicacion_me_gusta', 'publicacion_id', 'user_id')
+            ->withPivot('fecha_reaccion');
     }
 }
